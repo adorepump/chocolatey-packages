19b1f8cfcabab666557c67a106c8a1333f07bb83
Elliando Dias <adorepump@hotmail.com>
2012-05-22 17:12:32 -0400

    apache.ant package added

---
+++ apache.ant/tools/chocolateyInstall.ps1
@@ -0,0 +1,30 @@
+﻿# ant - v
+
+# Unable to locate tools.jar. Expected to find it in c:\program files (x86)\java\jre6\lib\tools.jar
+# JAVA_HOME > C:\Program Files (x86)\Java\jdk1.6.0_17
+
+function CreateFolder ([string]$Path) {
+  New-Item -Path $Path -type directory -Force
+}
+
+$binRoot = join-path $env:systemdrive 'bin'
+
+### Using an environment variable to to define the bin root until we implement YAML configuration ###
+if($env:chocolatey_bin_root -ne $null){
+  $binRoot = join-path $env:systemdrive $env:chocolatey_bin_root
+}
+
+CreateFolder($binRoot)
+
+$version = '1.8.3'
+$name = "apache-ant-$version" 
+$url = "http://linorg.usp.br/apache/ant/binaries/$name-bin.zip"
+$ant_home = Join-Path $binRoot $name
+$ant_bin = Join-Path  $ant_home 'bin'
+
+Install-ChocolateyZipPackage 'apache.ant' $url $binRoot
+
+[Environment]::SetEnvironmentVariable('ANT_HOME', $ant_home, "User")
+[Environment]::SetEnvironmentVariable('ANT_OPTS', '-Xmx256M', "User")
+
+Install-ChocolateyPath $ant_bin 'User' #add to path
