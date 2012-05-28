676c532aa8efcc87b76749701d07ea6f1454e8b3
Elliando Dias <adorepump@hotmail.com>
2012-05-28 10:52:33 -0400

    iReport package added

---
+++ iReport/tools/chocolateyInstall.ps1
@@ -0,0 +1,3 @@
+﻿$version = '4.5.1'
+$url = "http://ufpr.dl.sourceforge.net/project/ireport/iReport/iReport-$version/iReport-$version-windows-installer.exe"
+Install-ChocolateyPackage 'iReport' 'exe' '/S' $url -validExitCodes @(0)
