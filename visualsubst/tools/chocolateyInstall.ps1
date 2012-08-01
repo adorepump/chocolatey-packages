1be5fe5198cc7350c2e174e53b7a0a0aafadd73c
Elliando Dias <adorepump@hotmail.com>
2012-08-01 10:35:17 -0400

    visualsubst package added

---
+++ visualsubst/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+﻿#import-module C:\Chocolatey\chocolateyInstall\helpers\chocolateyInstaller
+
+$version = '1.0.6'
+Install-ChocolateyPackage 'visualsubst' 'exe' '/S' "http://www.ntwind.com/download/VSubst_$version.exe"  -validExitCodes @(0)
