ba0666b38eb4649b0303d0d161c5d4bfde9b4309
John Allers <john.allers@gmail.com>
2013-05-18 21:24:09 -0400

    Updated the Maven package to 3.0.5.  Added an uninstall script.

--- Maven/tools/chocolateyInstall.ps1
+++ Maven/tools/chocolateyInstall.ps1
@@ -7,14 +7,14 @@ function CreateFolder ([string]$Path) {
 
 $binRoot = join-path $env:systemdrive 'bin'
 
-### Using an environment variable to to define the bin root until we implement YAML configuration ###
+### Using an environment variable to define the bin root until we implement YAML configuration ###
 if($env:chocolatey_bin_root -ne $null){
   $binRoot = join-path $env:systemdrive $env:chocolatey_bin_root
 }
 
 CreateFolder($binRoot)
 
-$version = '3.0.4'
+$version = '3.0.5'
 $name = "apache-maven-$version"
 $m2_home = Join-Path $binRoot $name
 $m2_bin = Join-Path $m2_home 'bin'
@@ -23,6 +23,7 @@ $m2_repo = Join-Path $env:USERPROFILE '.m2'
 #$url = "http://apache.mirror.pop-sc.rnp.br/apache/maven/binaries/$name-bin.zip"
 #$url = "http://ftp.unicamp.br/pub/apache/maven/binaries/$name-bin.zip"
 $url = "http://www.bizdirusa.com/mirrors/apache/maven/maven-3/$version/binaries/$name-bin.zip"
+
 
 [Environment]::SetEnvironmentVariable('M2_HOME', $m2_home, "User")
 [Environment]::SetEnvironmentVariable('MAVEN_OPTS', '-Xms256m', "User")
---
+++ Maven/tools/chocolateyUninstall.ps1
@@ -0,0 +1,28 @@
+﻿$binRoot = join-path $env:systemdrive 'bin'
+
+### Using an environment variable to to define the bin root until we implement YAML configuration ###
+if($env:chocolatey_bin_root -ne $null){
+  $binRoot = join-path $env:systemdrive $env:chocolatey_bin_root
+}
+
+$version = '3.0.5'
+$name = "apache-maven-$version"
+$m2_home = Join-Path $binRoot $name
+$m2_bin = Join-Path $m2_home 'bin'
+$m2_repo = Join-Path $env:USERPROFILE '.m2'
+
+[Environment]::SetEnvironmentVariable('M2_HOME', $null, "User")
+[Environment]::SetEnvironmentVariable('MAVEN_OPTS', $null, "User")
+[Environment]::SetEnvironmentVariable('M2', $null, "User")
+[Environment]::SetEnvironmentVariable('M2_REPO', $null, "User")
+
+"Please manually remove Maven ($m2_home) from the PATH environment variable."
+# Remove Maven from the path environment variable
+#if ($null -ne $env:path)
+#{
+#   $p = $env:path.Split(";") |? {$_.toLower() -ne $m2_bin}
+#    $newPath = [String]::Join(";")
+#    [Environment]::SetEnvironmentVariable('PATH', $newPath, "User")
+#}
+
+Remove-Item $m2_home -Recurse -Force
