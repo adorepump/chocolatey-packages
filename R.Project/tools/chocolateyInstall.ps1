4ea4455a605e7f7bddf4c50c807f627184d3c836
Justin Dearing <zippy1981@gmail.com>
2014-01-07 22:26:02 -0500

    bunmped R.Project to 3.0.2

--- R.Project/tools/chocolateyInstall.ps1
+++ R.Project/tools/chocolateyInstall.ps1
@@ -1,2 +1,2 @@
-﻿$url='http://cran.rstudio.com/bin/windows/base/R-2.15.2-win.exe'
+﻿$url='http://cran.rstudio.com/bin/windows/base/R-3.0.2-win.exe'
 Install-ChocolateyPackage 'R.Project' 'exe' '/silent' "$url" -validExitCodes @(0)
