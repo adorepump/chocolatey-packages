fa8a948ab262c639f3b6ed3a59594c866a01eb51
Justin Dearing <zippy1981@gmail.com>
2014-01-07 22:41:26 -0500

    bunmped R.Studio to 0.98.490

--- R.Studio/tools/chocolateyInstall.ps1
+++ R.Studio/tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
-﻿$version = '0.95'
-$minor = '265'
+﻿$version = '0.98'
+$minor = '490'
 Install-ChocolateyPackage 'R.Studio' 'exe' '/S' "http://download1.rstudio.org/RStudio-$version.$minor.exe"  -validExitCodes @(0)
