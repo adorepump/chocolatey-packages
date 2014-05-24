8946cc2a22869b68bf5503a7c41096898b58b6ce
Justin Dearing <zippy1981@gmail.com>
2014-05-24 16:00:51 -0400

    bumped R.Project to 3.0.3

--- R.Project/tools/chocolateyUninstall.ps1
+++ R.Project/tools/chocolateyUninstall.ps1
@@ -1,2 +1,2 @@
-﻿$unpath="${Env:ProgramFiles}\R\R-3.0.2\unins000.exe"
+﻿$unpath="${Env:ProgramFiles}\R\R-3.0.3\unins000.exe"
 Uninstall-ChocolateyPackage 'R.Project' 'exe' '/silent' "$unpath" -validExitCodes @(0)
